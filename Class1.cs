513bc6305c79c4f7ee323b8f6f3d2742d5bbcf5c
opilane <opilane@HP-CZC6297B8Z>
2021-10-20 10:51:55 +0300

    5r

---
+++ Class1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kordamine_Valkrusman
+{
+    class Class1
+    {
+      
+        static void Saali_suurus()
+        {
+            Console.WriteLine("Vali saali suurus; 1,2,3");
+            int suurus = int.Parse(Console.ReadLine());
+            return suurus;
+
+        }
+        static int[,] saal = new int[,] { };
+        static int kohad, read;
+
+        static void Saali_taitmine(int suurus)
+        {
+            Random rnd = new Random();
+            if (suurus==1)
+            {
+                kohad = 20;
+                read = 10;
+            }
+            else if (suurus==2)
+            {
+                kohad = 20;read = 20;
+            }
+            else
+            {
+                kohad = 30;read = 20;
+            }
+            saal=new int 
+            for (int rida = 0; rida < read; rida++)
+            {
+                for (int koht = 0; koht < kohad; koht++)
+                {
+                    saal[rida, koht] = rnd.Next(0, 2);
+                }
+            }
+
+        }
+        static void Saal_ekraanile()
+        {
+            for (int rida = 0; rida < read; rida++)
+            {
+                for (int koht = 0; koht < kohad; rida++)
+                {
+                    Console.Write(saal[rida, koht]);
+
+                }
+                Console.WriteLine();
+              
+            }
+
+        }
+
+        static bool Muuk()
+        {
+            Console.Write("Rida: ");
+            int pileti_rida = int.Parse(Console.ReadLine());
+            Console.WriteLine("Koht:");
+            int pileti_koht = int.Parse(Console.ReadLine());
+            if (saal[pileti_rida-1,pileti_koht-1]==0)
+            {
+                saal[pileti_rida - 1, pileti_koht - 1] = 1;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+
+        static void Main(string[] args)
+        {
+            int suurus = Saali_suurus();
+            int[,] saal = new int[20, 30];
+            Random rnd = new Random();
+            for (int rida = 0; rida < 20; rida++)
+            {
+                for (int koht = 0; koht < 30; koht++)
+                {
+                    saal[rida, koht] = rnd.Next(0, 2);
+                }
+            }
+            for (int rida = 0; rida < 20; rida++)
+            {
+                for (int koht = 0; koht < 30; rida++)
+                {
+                    Console.WriteLine(saal[rida, koht]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
